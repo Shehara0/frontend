84d0934f04daf88509df62a0141e2db5f9d2b1ad
Shehara <sheharasiriwardana21@gmail.com>
2025-10-08 22:43:27 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import './App.css'
-import ProductCard from './components/productCard'
-import SuperProduct from './components/superProduct'
 
 function App() {
 
   return (
     <>
-      <div className='w-full h-screen bg-blue-100'>
-        <div className='w-[600px] h-[600px] bg-black flex flex-col  items-center justify-evenly' >
-        <div className='w-[75px] h-[75px] bg-yellow-500'></div>
-          <div className='w-[75px] h-[75px] bg-red-900'></div>
-            <div className='w-[75px] h-[75px] bg-green-600'></div>
-            <div className='w-[75px] h-[75px] bg-purple-600'></div>
-            <div className='w-[75px] h-[75px] bg-pink-600'></div>
-            <div className='w-[75px] h-[75px] bg-gray-600'></div>
-            <div className='w-[75px] h-[75px] bg-orange-600'></div>
-          </div>
-      </div>
+    
     </>
   )   
 }
